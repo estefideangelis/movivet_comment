f4ff00f548811611fa367d80f35efe3c8d0abb22
estefideangelis <essteffi@hotmail.com>
2017-11-27 20:52:38 -0300

    ama

--- js/post.js
+++ js/post.js
@@ -1,15 +1,14 @@
 $(document).bind('deviceready', function(){
     $(function(){
-		alert("entro");
-        $('form').submit(function(){
+		    $('form').submit(function(){
 			alert("entrodatadatos");
             var dataID = $(this).parent().attr('data-datos-id');
             var postData = $(this).serialize();
             $.ajax({
-                type: 'POST',
+				type: 'POST',
                 data: postData+'&lid='+dataID,
                 // cargamos la url del servidor externo
-                url: 'http://movivet.tk/app/guardar.php',
+                url: 'http://www.movivet.tk/app/guardar.php',
                 success: function(data){
                     console.log(data);
                     $('#email').val('');
